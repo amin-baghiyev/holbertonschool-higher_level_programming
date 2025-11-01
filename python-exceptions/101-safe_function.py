cffd0a297f6332a2cf0ea9bb2bf2d53e1ce96d13
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-01 18:45:52 +0000

    Safe function - code changed 3

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from sys import stderr
-
 def safe_function(fct, *args):
+    import sys
     try:
         return (fct(*args))
-    except Exception as e:
-        stderr.write("Exception: {}\n".format(e))
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
         return (None)
