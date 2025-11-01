1f67a34bc75197d5f7be1d8c0690e92fd665be09
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-01 18:28:15 +0000

    Safe integer print with error message - bug fixed

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception as e:
+        value = True
+    except (TypeError, ValueError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return (False)
-    return (True)
+        value = False
+    return (value)
