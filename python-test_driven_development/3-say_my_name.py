24bf244e66b79af3ee9e6c23baa58e1d2919b921
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-01 19:39:29 +0000

    Say my name - more cases added

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -11,4 +11,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+    if first_name is None:
+        raise TypeError("say_my_name() missing 1 required"
+                "positional argument: 'first_name")
     print("My name is {} {}".format(first_name, last_name))
