a34b3b3e19a18d4161ab267cc05b87c4fe5ead3f
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-01 18:44:37 +0000

    Safe function - code changed 2

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -3,8 +3,7 @@ from sys import stderr
 
 def safe_function(fct, *args):
     try:
-        r = fct(*args)
+        return (fct(*args))
     except Exception as e:
-        print("Exception: {}".format(e), file=stderr)
-        r = None
-    return (r)
+        stderr.write("Exception: {}\n".format(e))
+        return (None)
