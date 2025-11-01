774a66f28ecd2a503bf20f37571c437ac70575a5
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-01 18:22:15 +0000

    Safe integer print with error message - all exceptions are handled

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=stderr)
         return (False)
     return (True)
