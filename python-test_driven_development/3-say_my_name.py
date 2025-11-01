0d6720e25dbe74b074e81da7b92cadbc0f898bd1
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-01 19:43:38 +0000

    Say my name - just whatever requires

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -13,5 +13,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
     if first_name is None:
         raise TypeError("say_my_name() missing 1 required"
-                "positional argument: 'first_name")
+                        "positional argument: 'first_name")
     print("My name is {} {}".format(first_name, last_name))
