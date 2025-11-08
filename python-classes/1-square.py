d7ffc56f34efa678fc4e69856c1a2ecd18a90965
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-08 14:13:05 +0000

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains a Square class."""
+
+
+class Square():
+    """A class that represents a square."""
+
+    def __init__(self, size):
+        """Initialize the class"""
+        self.__size = size
