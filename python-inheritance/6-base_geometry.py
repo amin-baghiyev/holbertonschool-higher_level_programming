caef12a44ed4b3bccb72fba614006e36fee279c3
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-09 13:18:38 +0000

    Integer validator - doc edited

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -4,6 +4,7 @@
 
 class BaseGeometry():
     """A class that represents a geometric shapes."""
+
     def area(self):
         """Function that computes the area of the shape"""
         raise Exception("area() is not implemented")
