5be69fad9cb5d5ce37e5d868018b6ee0b997f2ad
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-09 12:51:33 +0000

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a BaseGeometry class."""
+
+
+class BaseGeometry():
+    """A class that represents a geometric shapes."""
+    def area(self):
+        """Function that computes the area of the shape"""
+        raise Exception("area() is not implemented")
