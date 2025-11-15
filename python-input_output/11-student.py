5421e562cf73de6bed93bec6b81aa526431357f6
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-15 18:04:22 +0000

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a Student class with JSON serialization"""
+
+
+class Student():
+    """Represent a student with first name, last name, and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary representation filtered by attrs if provided"""
+        if (attrs is None):
+            return (self.__dict__)
+        return ({k: v for k, v in self.__dict__.items() if k in attrs})
+
+    def reload_from_json(self, json):
+        """Replace attributes of the Student instance with values from json"""
+        for k, v in json.items():
+            setattr(self, k, v)
