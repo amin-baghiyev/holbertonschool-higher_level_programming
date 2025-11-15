5f26eb0917b5164f5cb4b61c81e6e4eb57021354
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-15 18:13:01 +0000

    Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Generate Pascal's Triangle up to a given number of rows"""
+
+
+def pascal_triangle(n):
+    """Generates Pascal's Triangle up to n."""
+    triangle = []
+    for i in range(n_rows):
+        row = [1]
+        if i > 0:
+            prev_row = triangle[i - 1]
+            for j in range(len(prev_row) - 1):
+                row.append(prev_row[j] + prev_row[j + 1])
+            row.append(1)
+        triangle.append(row)
+    return triangle
