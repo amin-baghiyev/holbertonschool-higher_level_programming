bb8a0699c58abe83167a68b49ca43fd7e37a2e45
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-15 17:22:06 +0000

    Load, add, save - some bugs fixed

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,8 +4,9 @@ from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+obj = argv[1:]
 try:
-    obj = load_from_json_file("add_item.json") + argv[1:]
+    obj += load_from_json_file("add_item.json")
 except FileNotFoundError:
-    obj = []
+    pass
 save_to_json_file(obj, "add_item.json")
