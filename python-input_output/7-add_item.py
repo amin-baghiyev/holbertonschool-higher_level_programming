1e067d2db236e308724e37218408d6eb3ac07516
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-15 17:14:36 +0000

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Add command-line arguments to a JSON file"""
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    obj = load_from_json_file("add_item.json") + argv[1:]
+except FileNotFoundError:
+    obj = []
+save_to_json_file(obj, "add_item.json")
