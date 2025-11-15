221b705f2c7de05e86e6e4037e3abcfa921567f3
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-15 19:09:45 +0000

    Converting CSV Data to JSON Format - data.json mentioned

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -10,7 +10,7 @@ def convert_csv_to_json(filename=""):
         with open(filename, 'r', encoding="utf-8") as csv_f:
             reader = csv.DictReader(csv_f)
             data = [r for r in reader]
-        with open(filename, 'w', encoding="utf-8") as json_f:
+        with open("data.json", 'w', encoding="utf-8") as json_f:
             json.dump(data, json_f)
         return True
     except Exception as e:
