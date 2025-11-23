2ab1e6941cdb44a184ec9e05680624c1a47d8cfb
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-23 18:00:09 +0000

    Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM states");
+
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+cursor.close()
+db.close()
