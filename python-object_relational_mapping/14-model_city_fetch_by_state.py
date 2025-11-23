a831fc20209124f038ab94b1154183262597298f
amin-baghiyev <baghiyev.amin@gmail.com>
2025-11-23 20:13:54 +0000

    Cities in state - errors are corrected

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""Module for fetching city by state"""
+"""prints all City objects from the database"""
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_city import Base, City
-from model_state import State
-from sys import argv
-
-# Run only when executed
 if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    from model_city import City
 
-    # Engine creation with mysql and mysqldb DBAPI
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(argv[1], argv[2], argv[3]))
-
-    # Creating all classes in DB
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1], argv[2], argv[3]
+    ))
     Base.metadata.create_all(engine)
-
-    # Creating Session and its instance
     Session = sessionmaker(bind=engine)
-    session = Session()
 
-    # The Query
+    session = Session()
     query = (
         session.query(State, City)
         .join(City, State.id == City.state_id)
         .order_by(City.id)
-        .all())
-
+        .all()
+    )
     for state, city in query:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+        print(f"{state.name}: ({city.id}) {city.name}")
 
     session.close()
